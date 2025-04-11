7895e0652b7842b51f169b69ccb3e82f060fec97
Isabel Melendez <lmelendezp@miumg.edu.gt>
2025-04-11 09:59:21 -0600

    Subiendo cambios

---
+++ PrototipoP3SP2025/src/main/java/Controlador/seguridad/Reservacion.java
@@ -0,0 +1,61 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador.seguridad;
+
+/**
+ *
+ * @author cdavi
+ */
+
+
+public class Reservacion {
+
+    public Reservacion() {
+    }
+
+    public Reservacion(String id_empresa, String nombre, String direccion, String telefono) {
+        this.id_empresa = id_empresa;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+    }
+
+    public String getId_empresa() {
+        return id_empresa;
+    }
+
+    public void setId_empresa(String id_empresa) {
+        this.id_empresa = id_empresa;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    private String id_empresa;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+
+}
